0324006bfdcd1ae5223b0371d5153b8fe16224cc
Ejean Kuo <ejeankuo2027@u.northwestern.edu>
2026-01-13 12:20:50 -0600

    update page2 background

--- frontend/src/pages/tsx/page2.tsx
+++ frontend/src/pages/tsx/page2.tsx
@@ -1,8 +1,18 @@
+import '../css/page2.css';
+import { useEffect } from 'react';
 
 export default function Page2() {
+    useEffect(() => {
+        // change background color just for this page
+        document.body.style.backgroundColor = "#F5D7A4";
+
+        return () => {
+            document.body.style.backgroundColor = "";
+        }
+    }, []);
+
     return (
-        <div className="page2">
-            <h1>page 2</h1>
-        </div>
+        // placeholder content
+        <h1 className="title">Page 2</h1>
     )
 }
